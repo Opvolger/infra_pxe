e7f8c01d3520372edaea86234471845875a4eeae
Bas M <opvolger@gmail.com>
2020-09-25 15:56:43 +0200

    compile ipxe in docker

--- docker_image/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy files from compiled code
-cp ../compiled/ipxe/src/bin/undionly.kpxe files/ipxe
-cp ../compiled/ipxe/src/bin-x86_64-efi/ipxe.efi files/ipxe
-# deze stap moetnag naar ansible
-wget -N https://git.ipxe.org/releases/wimboot/wimboot-latest.zip -P ../download
-unzip -o ../download/wimboot-latest.zip -d files
-docker build -t opvolger/pxe:latest .
