7a49341b8f23c64933e84206a5b8ed3bec6c19ea
Bas M <opvolger@gmail.com>
2020-10-05 21:35:39 +0200

    supervisor gebruiken

--- docker_image/files/docker-entrypoint.sh
+++ docker_image/files/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# copy ipxe en wimboot files to volume 
+# copy ipxe and wimboot files to volume
+echo Copy ipxe and wimboot
 cp /files/ipxe/* /pxe/ipxe
 
 # create dnsmasq files (if nog exists)
@@ -9,21 +10,16 @@ FILE=/pxe/dnsmasq/dnsmasq.conf
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
+    echo create dnsmasq.conf
     # create etc/dnsmasq.conf and set DHCP_RANGE 
     sed 's|{{ DHCP_RANGE }}|'$DHCP_RANGE'|g' /templates/dnsmasq.conf > /pxe/dnsmasq/dnsmasq.conf
     # replace PXE_IP_ADDRESS
     sed -i 's|{{ PXE_IP_ADDRESS }}|'$PXE_IP_ADDRESS'|g' /pxe/dnsmasq/dnsmasq.conf
 fi
 
-# start nginx (background)
-nginx
-echo "nginx Started"
-
 # create sock for php
+echo Create sock for php
 touch /run/php-fpm/www.sock
-# start php-fpm (background)
-php-fpm
-echo "php-fpm Started"
 
-# --no0daemon zodat deze zichtbaar blijft draaien
-dnsmasq --no-daemon --conf-file=/pxe/dnsmasq/dnsmasq.conf
+echo Start the supervisord
+supervisord -c /etc/supervisord.conf
